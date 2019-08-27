da1aa8a4b14d6cedec1eb67d346ad1a9e9e6c3cf
AaronShah2 <47093449+AaronShah2@users.noreply.github.com>
2019-08-27 08:09:29 -0700

    Updated Dashboard UI
    Updated Dashboard UI

--- src/app/components/dashboard-template/DashBoardTemplate.js
+++ src/app/components/dashboard-template/DashBoardTemplate.js
@@ -1,22 +1,95 @@
-import React from "react";
-
-import SideNav from "../../components/sidenav/SideNav";
-import TopNav from "../../components/topnav/TopNav";
+import React from'react';
+import './style.scss';
+import { ToastContainer, toast, cssTransition } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import kontessLogoImg from 'assets/images/logo.png';
 import Footer from "../../components/footer/Footer";
-import "./style.scss";
 
+import dashboardIcon from "assets/icons/dashboard.svg";
+import eventIcon from "assets/icons/activity.svg";
+import userIcon from "assets/icons/activity.svg";
+
+toast.configure() 
+const pages = [
+  {
+    name: "Dashboard",
+    iconPath: dashboardIcon,
+    pageId: "dashboard-nav"
+  },
+  {
+    name: "Event",
+    iconPath: eventIcon,
+    pageId: "event-nav"
+  },
+  {
+    name: "Users",
+    iconPath: userIcon,
+    pageId: "users-nav"
+  }
+];
 export default class DashboardTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPageId: props.currentPageId
+    };
+    this.notify = this.notify.bind(this);
+  }
+
+  notify = () => toast("You have no notifications.", {
+    draggablePercent: 60,
+    closeButton: false,
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    position: toast.POSITION.TOP_CENTER
+  });
+
+  getButtons() {
+    const buttons = pages.map(page => (
+      <button
+        className={
+          "nav-button" + (page.isActive ? " sidenav-button-active" : "")
+        }
+        id={page.pageId}
+        onClick={event => {
+          this.switchPage(event, page.pageId);
+        }}
+      >
+        <span className="icon-text-wrapper">
+          <img className="icon-img" src={page.iconPath} alt="" />
+          <p>{page.name}</p>
+        </span>
+      </button>
+    ));
+    return <div>{buttons}</div>;
+  }
+  switchPage(event, targetPageId) {
+    // TODO: implement page switching
+    event.preventDefault();
+  }
   render() {
     return (
-      <div>
-        <div className="dashboard">
-          <SideNav currentPageId="dashboard-nav" />
-          <div className="rightColumn">
-            <TopNav />
-            {this.props.children}
-          </div>
+      <div className = "dashboard">
+        <div className = {"title-block"}>
+          <h1 className = {"title"}>
+            {"Team Information"}
+          </h1>
+          <button 
+            className = {"notification"}
+            onClick = {this.notify}
+          >
+            N
+          </button>
         </div>
-        <Footer />
+        <div className= 'sidebar'>
+          <img src={kontessLogoImg} className= "logo"></img>
+          <h6 className = "main-menu">Main Menu</h6>
+          {(() => this.getButtons())()}
+        </div>
+        <Footer/>
       </div>
     );
   }
