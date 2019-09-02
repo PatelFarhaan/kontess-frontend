ca559ef4038706e23ba7e5796723fc3586449698
Kevin <kevinfang28@gmail.com>
2019-09-02 10:25:52 -0700

    modularize table

--- src/app/containers/teamlist/TeamList.js
+++ src/app/containers/teamlist/TeamList.js
@@ -4,14 +4,10 @@ import Modal from "react-modal";
 import DashboardTemplate from "../../components/dashboard-template/DashBoardTemplate";
 import CreateTeamForm from "../../components/forms/CreateTeamForm";
 import TeamRequestForm from "../../components/forms/TeamRequestForm";
-import TeamService from "../../services/TeamService";
+import TeamListTable from "../../components/tables/TeamListTable";
 import deleteIcon from "assets/icons/delete.svg";
-import previousButton from "assets/images/pagePrevious.png";
-import nextButton from "assets/images/pageNext.png";
 
-import * as api from "../../../utils/requests";
 import * as session from "../../../utils/session";
-import * as routes from "../../globals/endpoints";
 
 import "./style.scss";
 
@@ -29,70 +25,10 @@ export default class TeamList extends React.Component {
       page: 0
     };
 
-    this.createOrganizerRows = this.createOrganizerRows.bind(this);
-    this.createParticipantRows = this.createParticipantRows.bind(this);
     this.openCreateModal = this.openCreateModal.bind(this);
     this.closeCreateModal = this.closeCreateModal.bind(this);
     this.openTeamRequestModal = this.openTeamRequestModal.bind(this);
     this.closeTeamRequestModal = this.closeTeamRequestModal.bind(this);
-    this.getTeamList = this.getTeamList.bind(this);
-    this.setPage = this.setPage.bind(this);
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
-  }
-
-  componentDidMount() {
-    this.getTeamList(this.state.page);
-  }
-
-  setPage(page) {
-    this.setState({ page: page });
-    this.getTeamList(page * 10);
-  }
-
-  nextPage() {
-    if (this.state.page + 1 < this.state.maxPage) {
-      this.setPage(this.state.page + 1);
-    }
-  }
-
-  previousPage() {
-    if (this.state.page > 0) {
-      this.setPage(this.state.page - 1);
-    }
-  }
-
-  getTeamList(offset) {
-    return api
-      .getRoute(routes.teamRoute + "?limit=10&offset=" + offset)
-      .then(d => {
-        const allTeamData = [];
-        d.data.results.forEach(element => {
-          allTeamData.push(this.parseTeam(element));
-        });
-        console.log(allTeamData);
-        this.setState({
-          teams: allTeamData,
-          nextUrl: d.data.next,
-          previousUrl: d.data.previous,
-          maxPage: Math.ceil(d.data.count / 10)
-        });
-      });
-  }
-
-  // team data parsing functions
-  parseTeam(team) {
-    const teamData = {};
-    const id = team["id"];
-    const name = team["name"];
-    const description = team["description"];
-    const participants = team["participants"];
-
-    teamData["id"] = id;
-    teamData["name"] = name;
-    teamData["description"] = description;
-    teamData["participants"] = participants;
-    return teamData;
   }
 
   // modal functions
@@ -106,90 +42,13 @@ export default class TeamList extends React.Component {
   }
 
   openTeamRequestModal(id) {
+    console.log(id);
     this.setState({ teamRequestModalIsOpen: true, joinRequestTeamId: id });
   }
 
   closeTeamRequestModal() {
     this.setState({ teamRequestModalIsOpen: false });
     window.location.reload();
-  }
-
-  deleteTeam(id) {
-    TeamService.delete(id)
-      .then(d => {
-        window.location.reload();
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  joinTeam(id) {
-    TeamService.joinTeam(id)
-      .then(d => {
-        window.location.reload();
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  createPageNumbers() {
-    let index = this.state.page;
-    if (index == 0) {
-      index = 1;
-    }
-    let rows = [];
-    for (var i = -1; i < 2; i++) {
-      const pageNum = index + i;
-      if (pageNum >= 0 && pageNum < this.state.maxPage) {
-        rows.push(
-          <button className="pageButton" onClick={() => this.setPage(pageNum)}>
-            {pageNum}
-          </button>
-        );
-      }
-    }
-
-    return <span className="pageNum"> {rows} </span>;
-  }
-
-  createOrganizerRows() {
-    const rows = this.state.teams.map(team => (
-      <tr>
-        <td>{team["name"]}</td>
-        <td>{team["description"]}</td>
-        <td>{team["participants"].length}</td>
-        <td>
-          <button
-            className="delete-button"
-            onClick={() => this.deleteTeam(team["id"])}
-          >
-            <img src={deleteIcon} alt="delete" />
-          </button>
-        </td>
-      </tr>
-    ));
-    return <> {rows} </>;
-  }
-
-  createParticipantRows() {
-    const rows = this.state.teams.map(team => (
-      <tr>
-        <td>{team["name"]}</td>
-        <td>{team["description"]}</td>
-        <td>{team["participants"].length}</td>
-        <td>
-          <button
-            className="join-button"
-            onClick={() => this.openTeamRequestModal(team["id"])}
-          >
-            Join
-          </button>
-        </td>
-      </tr>
-    ));
-    return <> {rows} </>;
   }
 
   render() {
@@ -201,30 +60,19 @@ export default class TeamList extends React.Component {
               Create Team
             </button>
           </div>
-          <table className="table">
-            <tr>
-              <th width="30%">Name</th>
-              <th width="62%">Description</th>
-              <th width="5%">Size</th>
-              {session.getUserType() == "organizer" ? (
-                <th width="3%"></th>
-              ) : (
-                <div></div>
-              )}
-            </tr>
-            {session.getUserType() == "organizer" && this.createOrganizerRows()}
-            {session.getUserType() == "participant" &&
-              this.createParticipantRows()}
-          </table>
-          <div className="button-container">
-            <button className="pageButton" onClick={this.previousPage}>
-              <img src={previousButton} />
-            </button>
-            {this.createPageNumbers()}
-            <button className="pageButton" onClick={this.nextPage}>
-              <img src={nextButton} />
-            </button>
-          </div>
+          {session.getUserType() == "organizer" ? (
+            <TeamListTable
+              openTeamRequestModal={this.openTeamRequestModal}
+              openCreateModal={this.openCreateModal}
+            />
+          ) : session.getUserType() == "participant" ? (
+            <TeamListTable
+              openTeamRequestModal={this.openTeamRequestModal}
+              openCreateModal={this.openCreateModal}
+            />
+          ) : (
+            <> </>
+          )}
           <Modal
             isOpen={this.state.createModalIsOpen}
             onRequestClose={this.closeCreateModal}
