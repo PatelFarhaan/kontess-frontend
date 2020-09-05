8e159024bc548598465f77d827a9056e90ea1860
Darryl Ferdinands <darrylferdinands@gmail.com>
2020-09-04 20:21:15 -0700

    build changes

---
+++ latest/build/precache-manifest.f23f61cbc2f12318aead6d20aaac4c69.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a89a9feffc6d04b653d208b49df989",
+    "url": "/kontess/index.html"
+  },
+  {
+    "revision": "ae8b01ea1f64034fb41b",
+    "url": "/kontess/static/css/2.99604850.chunk.css"
+  },
+  {
+    "revision": "400b71d8d015a4f81228",
+    "url": "/kontess/static/css/main.3da84ab9.chunk.css"
+  },
+  {
+    "revision": "ae8b01ea1f64034fb41b",
+    "url": "/kontess/static/js/2.aa956ad8.chunk.js"
+  },
+  {
+    "revision": "e1f7ded4046699066e2f06dd86665697",
+    "url": "/kontess/static/js/2.aa956ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400b71d8d015a4f81228",
+    "url": "/kontess/static/js/main.b5d558c3.chunk.js"
+  },
+  {
+    "revision": "238abb5e957e6b660e9f",
+    "url": "/kontess/static/js/runtime-main.2682799b.js"
+  },
+  {
+    "revision": "4850785ff4b5536a80f0e2bd977ea7d6",
+    "url": "/kontess/static/media/Montserrat-Medium.4850785f.woff"
+  },
+  {
+    "revision": "ab459481bb83f2cf53e5da9d7067323b",
+    "url": "/kontess/static/media/Montserrat-Regular.ab459481.woff"
+  },
+  {
+    "revision": "87af7cb84fd76ccadb833df27b850a9c",
+    "url": "/kontess/static/media/OpenSans-Bold.87af7cb8.woff"
+  },
+  {
+    "revision": "35419e354261638f1c8ca2f3ec069c90",
+    "url": "/kontess/static/media/OpenSans-Regular.35419e35.woff"
+  },
+  {
+    "revision": "4ffd5b96952a343d253c3c7ccd8d448d",
+    "url": "/kontess/static/media/background2.4ffd5b96.jpg"
+  },
+  {
+    "revision": "a5447cc333e631c94f4a64c3e951af74",
+    "url": "/kontess/static/media/bgSMU.a5447cc3.jpg"
+  },
+  {
+    "revision": "139aeefb2b57eb26bc62db464f573878",
+    "url": "/kontess/static/media/profile.139aeefb.svg"
+  }
+]);
