f8a75e2bd1e221b8e325b94ed152162830530b4c
Jiahan Cong <congj1@uci.edu>
2020-12-23 21:01:02 -0800

    tech req no.5 1st commit

--- latest/src/app/containers/UserDashBoard/to-do-list.js
+++ latest/src/app/containers/UserDashBoard/to-do-list.js
@@ -50,7 +50,7 @@ export default class ToDOList extends React.Component {
     render() {
         const { userId, type, userType } = this.state;
         return (
-            <div className="todo-list-section mt-5">
+            <div className="todo-list-section mb-5">
                 <h5 className="mb-4 text-muted">{this.props.title}</h5>
                 {type === 'byTeam' ?
                     <div>
